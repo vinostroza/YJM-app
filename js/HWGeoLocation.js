b545acd2882aaab1660fb47dd13783314bf97390
Wennona <50216937+WWWeLLs@users.noreply.github.com>
2019-06-01 14:37:13 -0500

    Add files via upload

---
+++ js/HWGeoLocation.js
@@ -0,0 +1,32 @@
+﻿function myLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(myPosition);
+    } else {
+
+        alert('GeoLocation is NOT supported ');
+    }
+
+}
+function myPosition(position) {
+    var location = {
+        lat: 43.0718,
+        lng: 12.6135
+        //TESTING DIFFERENT COORDINATES LOCATIONA - ITALY
+    };
+
+    //map
+    var map = new google.maps.Map(
+        document.getElementById('map'),
+        { zoom: 20, center: location }
+
+    );
+
+    //marker
+    var marker = new google.maps.Marker(
+        {
+            position: location,
+            map: map
+        }
+
+    )
+}
